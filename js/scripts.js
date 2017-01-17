61788ff19aa06dd05d56997be60cdb738d9976c8
Kaz Matthews and said ahmed <said@>
2017-01-17 15:53:51 -0800

    add variable to store number value of inputed number. begin to add jquery functionality.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#factorial").submit(function(event){
+    event.preventDefault();
+
+    var inputNumber = parseInt($("input#number1").val());
+
+    for(i=inputNumber; i>0; i--)
+
+  });
+});
