dc1baf3333bb3c29f2645d7608422c39a63cb504
Kaz Matthews and said ahmed <said@>
2017-01-17 16:41:11 -0800

    move factorial function into bussinus logic and create factorial function to make sure it includes parameter of number1

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+var factorial = function(number1) {
+  var startingNumber = 1;
+
+  for(i=number1; i>0; i--) {
+    startingNumber *= i;
+  }
+  return startingNumber;
+}
+
 $(document).ready(function(){
   $("#factorial").submit(function(event){
     event.preventDefault();
 
     var inputNumber = parseInt($("input#number1").val());
 
-    for(i=inputNumber; i>0; i--)
+    $(".result").show();
+    $("#result").text(factorial(inputNumber));
 
   });
 });
